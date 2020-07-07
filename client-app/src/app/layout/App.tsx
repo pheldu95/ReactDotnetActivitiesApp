83976e9ecc6133ddb6a71ceb9ac7a6d060bf6171
Duncan Phelps <phelps.duncan@gmail.com>
2020-07-07 14:32:41 -0500

    converted our App class component into a functional component, so we can use react hooks. mainly useState.

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,44 +1,37 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Header, Icon, List } from 'semantic-ui-react';
 import axios from 'axios';
 import { IActivity } from '../models/activity';
 
-interface IState {
-  activities: IActivity[]
-}
 
-class App extends Component<{}, IState> {
-  //tell our state it is of type IState, which we defined above
-  //types defined in IActivity will now be checked
-  //make it readonly. so only way we can modify it is by using this.setState
-  readonly state: IState = {
-    activities: []
-  };
-
-  componentDidMount() {
+const App = () => {
+  //we use useState from react to create our state
+  const [activities, setActivities] = useState<IActivity[]>([]);
+  
+  //no longer need componentDidMount
+  useEffect(()=> {
     //add <IActivity[]> after the .get, telling that it should be getting an array of IActivities
     axios.get<IActivity[]>('http://localhost:5000/api/activities').then(response => {
-      this.setState({
-        activities: response.data
-      });
+      //insted of using this.setState, use setActivities
+      setActivities(response.data);
     });
-  }
+    //the empty array added at the end ensures our useEffect onlky runs one time, instead of continuously
+  }, [])
 
-  render() {
-    return (
-      <div>
-        <Header as='h2'>
-          <Icon name='users' />
-          <Header.Content>Reactivities</Header.Content>
-        </Header>
-        <List>
-          {this.state.activities.map((activity) => (
-            <List.Item key={activity.id}>{activity.title}</List.Item>
-          ))}
-        </List>
-      </div>
-    );
-  }
+  return (
+    <div>
+      <Header as='h2'>
+        <Icon name='users' />
+        <Header.Content>Reactivities</Header.Content>
+      </Header>
+      <List>
+        {activities.map((activity) => (
+          <List.Item key={activity.id}>{activity.title}</List.Item>
+        ))}
+      </List>
+    </div>
+  );
 }
+
 
 export default App;
